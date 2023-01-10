e87192bd7436062aeff61889d1ac3b39589b6120
keksik95 <geliashvilidato95@gmail.com>
2023-01-10 17:39:53 +0400

    tabs and table version

--- src/components/Dropdown/Dropdown.jsx
+++ src/components/Dropdown/Dropdown.jsx
@@ -1,59 +1,65 @@
 import "./Dropdown.css";
 
 export const Dropdown = (props) => {
-    let element = '';
-    if (props.type === 'country') {
-        element =
-            <div className={props.dropdown}>
-                {props.countryData.map((item, index) => {
-                    return (
-                        <div key={index}>
-                            <p>
-                                <img src={`${item.image}`} alt="country" />
-                                {item.title}
-                                {item.numbering}
-                            </p>
-                        </div>
-                    )
-                })}
+  let element = "";
+  if (props.type === "country") {
+    element = (
+      <div className={props.dropdown}>
+        {props.countryData.map((item, index) => {
+          return (
+            <div key={index}>
+              <p>
+                <img src={`${item.image}`} alt="country" />
+                {item.title}
+                {item.numbering}
+              </p>
             </div>
-    }
-    if (props.type === 'dropdown') {
-        element =
-            <div className={props.dropdown}>
-                {props.data.map((item, index) => {
-                    return (
-                        <div className="lastChild" key={index}>
-                            <h1 className="titles">
-                                {item.title}
-                            </h1>
-                            {item.list.map((item, index) => {
-                                return (
-                                    <div key={index} className={props.dropdownItem}>
-                                        {/* <p className={props.active === `${item.id}` ? "border" : ""}></p> */}
-                                        <span className={item.id < 3 ? "filter" : ''}>
-                                            {item.svg}
-                                        </span>
-                                        <div className={props.containerCheck}>
-                                            <p id={item.id} onClick={props.onClick} className={`${props.active === `${item.id}` ? "rightLine" : ''}`}>
-                                                {item.title}
-                                            </p>
-                                            {item.togle === 'true' &&
-                                                <div className="checkboxContainer">
-                                                    <input type="checkbox" value="false" />
-                                                    <div className="checkbox">
-                                                        <i></i>
-                                                    </div>
-                                                </div>
-                                            }
-                                        </div>
-                                    </div>
-                                )
-                            })}
+          );
+        })}
+      </div>
+    );
+  }
+  if (props.type === "dropdown") {
+    element = (
+      <div className={props.dropdown}>
+        {props.data.map((item, index) => {
+          return (
+            <div className="lastChild" key={index}>
+              <h1 className="titles">{item.title}</h1>
+              {item.list.map((item, index) => {
+                return (
+                  <div key={index} className={props.dropdownItem}>
+                    {/* <p className={props.active === `${item.id}` ? "border" : ""}></p> */}
+                    <span className={item.id < 3 ? "filter" : ""}>
+                      {item.svg}
+                    </span>
+                    <div className={props.containerCheck}>
+                      <p
+                        id={item.id}
+                        onClick={props.onClick}
+                        className={`${
+                          props.active === `${item.id}` ? "rightLine" : ""
+                        }`}
+                      >
+                        {item.title}
+                      </p>
+                      {item.togle === "true" && (
+                        <div className="checkboxContainer">
+                          <input type="checkbox" value="false" />
+                          <div className="checkbox">
+                            <i></i>
+                          </div>
                         </div>
-                    )
-                })}
+                      )}
+                    </div>
+                  </div>
+                );
+              })}
             </div>
-    }
-    return element;
-}
+          );
+        })}
+      </div>
+    );
+  }
+  return element;
+};
--- src/stories/Tabs.stories.js
+++ src/stories/Tabs.stories.js
@@ -84,6 +84,46 @@ stories.add("Tabs", () => {
             >
               Payments <span>0</span>
             </div>
+            <div className="expend-svg">
+              <svg
+                width="16"
+                height="18"
+                viewBox="0 0 16 18"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M0.5 17.3333H15.5"
+                  stroke="#9C9DA3"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+                <path
+                  d="M0.5 0.666626H15.5"
+                  stroke="#9C9DA3"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+                <path
+                  d="M8 4V14"
+                  stroke="#9C9DA3"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+                <path
+                  d="M10.3583 5.43328L7.99993 3.07495L5.6416 5.43328"
+                  stroke="#9C9DA3"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+                <path
+                  d="M10.3583 12.2416L7.99993 14.5999L5.6416 12.2416"
+                  stroke="#9C9DA3"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </svg>
+            </div>
           </>
         }
       />
