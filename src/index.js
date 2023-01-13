4062f5f4964aaea14c5633b2ecd991ae82f407f3
keksik95 <geliashvilidato95@gmail.com>
2023-01-13 15:35:58 +0400

    fixing

--- src/index.js
+++ src/index.js
@@ -1,9 +1,4 @@
-export * from './components/Button';
-export * from './components/Input';
-export * from './components/HelpText';
-<<<<<<< HEAD
-export * from './components/InfoBox';
-
-=======
-export * from './components/Dropdown';
->>>>>>> e87192bd7436062aeff61889d1ac3b39589b6120
+export * from "./components/Button";
+export * from "./components/Input";
+export * from "./components/HelpText";
+export * from "./components/Dropdown";
