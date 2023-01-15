9379b380a9161082724be515eee1d040afe02ac5
sergo93@gmail.com <sergo93@gmail.com>
2023-01-15 18:16:46 +0400

    sidebar

---
+++ src/stories/Visual.stories.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import { storiesOf } from "@storybook/react";
+import "../assets/css/main-theme.css";
+import { Visual } from "../components/Visual";
+
+const stories = storiesOf("Visual", module);
+
+stories.add("Visual", () => {
+    return (
+        <div style={{marginLeft: '100px'}}>
+            <Visual
+                label={'User Account'}
+                element={'popup-header'}
+                customStyles={{width: '340px'}}
+            />
+            <Visual
+                label={'or'}
+                element={'delimiter'}
+                customStyles={{width: '340px'}}
+            />
+            <Visual
+                label={'0xb81G555828610c156eF3408DE21A3eC9b9B29V81'}
+                element={'copy-address'}
+                customStyles={{width: '340px'}}
+            />
+        </div>
+    );
+});
