120c5f3454cf993f90faf067493117eb6b1e11df
bezhan kalichava <bekalichava22@gmail.com>
2023-01-23 12:31:06 +0400

    update

--- src/components/ExtensionCard/ExtensionCard.jsx
+++ src/components/ExtensionCard/ExtensionCard.jsx
@@ -2,12 +2,9 @@ import "./ExtensionCard.css";
 import { Switches } from "../Switches";
 
 export const ExtensionCard = (props) => {
-    const arrowSvg = <svg xmlns="http://www.w3.org/2000/svg" width="13" height="16" viewBox="0 0 13 16" fill="none">
-        <path d="M1.33333 1.70095L5.16097 5.52859C5.61301 5.98063 5.61301 6.72032 5.16097 7.17236L1.33333 11" stroke={`${props.type === "trade" ? "#3D5AFE" : "#FFF"}`} strokeWidth="1.5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round" />
-    </svg>
 
     return (
-        <div className={`extension-card ${props.type} ${props.active === true ? "animation" : ""}`}>
+        <div style={props.customStyle} className={`extension-card ${props.type} ${props.active === true ? "animation" : ""}`}>
             {props.type === "staking" ? (
                 <div className="staking-card">
                     <div className="staking-card-header active">
@@ -15,13 +12,13 @@ export const ExtensionCard = (props) => {
                         <h1>{props.cardBody.h}</h1>
                     </div>
                     <div className="extension-card-body active">
-                         {arrowSvg}
+                         {props.cardBody.arrow}
                     </div>
                 </div>
-            ) : (
+            ) : (props.type === "loan" || props.type === "trade") ? (
                 <>
                     <div className="card-header active">
-                        <img src={props.cardBody.icon} alt="#" />
+                        {props.cardBody.icon}
                         <Switches
                             type={'lg-switches'}
                             size={'size'}
@@ -30,12 +27,50 @@ export const ExtensionCard = (props) => {
                     <div className="extension-card-body active">
                         <div className="extension-card-body-header">
                             <h1>{props.cardBody.h}</h1>
-                            {arrowSvg}
+                            {props.cardBody.arrow}
                         </div>
                         <p className="font-16">{props.cardBody.p}</p>
                     </div>
                 </>
-            )
+            ) : props.type === "extension-component" ? (
+                <div className="extension-component-wrapp">
+                    <div style={{padding: "35px", backgroundColor: "#191D2B", borderRadius: "8px"}} className="card-header active">
+                        {props.cardBody.image}
+                    </div>
+                    <div className="extension-component-content">
+                        <div className="extension-component-header extension-card-body active">
+                            <h1>{props.cardBody.title}</h1>
+                            <div className="hushCode">
+                                <p>{props.cardBody.hush}</p>  
+                                {props.cardBody.arrow}  
+                            </div>
+                        </div>
+                        <div className="extension-component-body extension-card-footer active">
+                            <div className="component-body-section">
+                                <h4>{props.cardBody.contentOne.title}</h4>
+                                <div className="component-body-section-body">
+                                    <p>{props.cardBody.contentOne.subTitle}</p>
+                                    {props.cardBody.contentOne.contentSvg}
+                                </div>
+                            </div>
+                            <div className="line"></div>
+                            <div className="component-body-section component-body-section">
+                                <h4>{props.cardBody.contentTwo.title}</h4>
+                                <div className="component-body-section-body">
+                                    <p>{props.cardBody.contentTwo.usd}</p>
+                                    {props.cardBody.contentTwo.dot}
+                                    <p>{props.cardBody.contentTwo.currency}</p>
+                                </div>
+                            </div>
+                            <div className="line"></div>
+                            <div className="component-body-section">
+                                <h4>{props.cardBody.contentThree.title}</h4>
+                                {props.cardBody.contentThree.svg}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            ) : ""
             }
         </div>
     )
