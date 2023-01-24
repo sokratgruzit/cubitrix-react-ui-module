f54b82fb9e692347fd5d52dc18c2d3218ecdc2ed
keksik95 <geliashvilidato95@gmail.com>
2023-01-24 16:02:56 +0400

    update

--- src/components/TableElement/TableElement.jsx
+++ src/components/TableElement/TableElement.jsx
@@ -8,72 +8,7 @@ export const TableElement = (props) => {
     setActive(num);
   };
 
-  // function Pagination({ data }) {
-  //   let { total, limit, page, hanldeChange } = data;
-  //   let count = Math.ceil(Number(total) / limit);
-  //   let buttons = [];
-  //   for (let i = 1; i <= count; i++) {
-  //     buttons.push(
-  //       <button
-  //         className={`item ${page == i ? "active" : ""}`}
-  //         key={i}
-  //         onClick={() => hanldeChange("page", i)}
-  //       >
-  //         {i}
-  //       </button>
-  //     );
-  //   }
-
-  //   return (
-  //     <div className="pagination">
-  //       <button
-  //         className={`item prev ${page === 1 ? "disabled" : ""}`}
-  //         disabled={page === 1}
-  //         onClick={() => hanldeChange("page", page - 1)}
-  //       ></button>
-  //       {buttons}
-  //       <button
-  //         className={`item next ${count === page ? "disabled" : ""}`}
-  //         disabled={count === page}
-  //         onClick={() => hanldeChange("page", page + 1)}
-  //       ></button>
-  //     </div>
-  //   );
-  // }
-
   let element = null;
-
-  // if (props.type === "search") {
-  //   element = (
-  //     <div>
-  //       <div className="quick-search">
-  //         <input placeholder={props.placeholder} style={props.customStyles} />
-  //         <svg
-  //           width="16"
-  //           height="16"
-  //           viewBox="0 0 16 16"
-  //           fill="none"
-  //           xmlns="http://www.w3.org/2000/svg"
-  //         >
-  //           <path
-  //             d="M7.65 14.3C8.52329 14.3 9.38803 14.128 10.1948 13.7938C11.0017 13.4596 11.7348 12.9698 12.3523 12.3523C12.9698 11.7348 13.4596 11.0017 13.7938 10.1948C14.128 9.38803 14.3 8.52329 14.3 7.65C14.3 6.77671 14.128 5.91197 13.7938 5.10515C13.4596 4.29834 12.9698 3.56525 12.3523 2.94774C11.7348 2.33023 11.0017 1.84039 10.1948 1.5062C9.38803 1.17201 8.52329 1 7.65 1C5.88631 1 4.19486 1.70062 2.94774 2.94774C1.70062 4.19486 1 5.88631 1 7.65C1 9.41369 1.70062 11.1051 2.94774 12.3523C4.19486 13.5994 5.88631 14.3 7.65 14.3V14.3Z"
-  //             stroke="white"
-  //             strokeWidth="1.5"
-  //             strokeLinecap="round"
-  //             strokeLinejoin="round"
-  //           />
-  //           <path
-  //             d="M15 15.0001L13.6 13.6001"
-  //             stroke="white"
-  //             strokeWidth="1.5"
-  //             strokeLinecap="round"
-  //             strokeLinejoin="round"
-  //           />
-  //         </svg>
-  //       </div>
-  //     </div>
-  //   );
-  // }
 
   if (props.type === "pagination") {
     element = (
@@ -102,7 +37,7 @@ export const TableElement = (props) => {
               onClick={() => {
                 pageHandler(1);
               }}
-              className={`${active === 1 ? "active" : ""}`}
+              className={`${active === 1 ? "active-element" : ""}`}
             >
               1
             </div>
@@ -110,7 +45,7 @@ export const TableElement = (props) => {
               onClick={() => {
                 pageHandler(2);
               }}
-              className={`${active === 2 ? "active" : ""}`}
+              className={`${active === 2 ? "active-element" : ""}`}
             >
               2
             </div>
@@ -118,7 +53,7 @@ export const TableElement = (props) => {
               onClick={() => {
                 pageHandler(3);
               }}
-              className={`${active === 3 ? "active" : ""}`}
+              className={`${active === 3 ? "active-element" : ""}`}
             >
               3
             </div>
@@ -126,7 +61,7 @@ export const TableElement = (props) => {
               onClick={() => {
                 pageHandler(4);
               }}
-              className={`${active === 4 ? "active" : ""}`}
+              className={`${active === 4 ? "active-element" : ""}`}
             >
               4
             </div>
@@ -135,7 +70,7 @@ export const TableElement = (props) => {
               onClick={() => {
                 pageHandler(5);
               }}
-              className={`${active === 5 ? "active" : ""}`}
+              className={`${active === 5 ? "active-element" : ""}`}
             >
               97
             </div>
@@ -143,7 +78,7 @@ export const TableElement = (props) => {
               onClick={() => {
                 pageHandler(6);
               }}
-              className={`${active === 6 ? "active" : ""}`}
+              className={`${active === 6 ? "active-element" : ""}`}
             >
               98
             </div>
@@ -151,7 +86,7 @@ export const TableElement = (props) => {
               onClick={() => {
                 pageHandler(7);
               }}
-              className={`${active === 7 ? "active" : ""}`}
+              className={`${active === 7 ? "active-element" : ""}`}
             >
               99
             </div>
@@ -159,7 +94,7 @@ export const TableElement = (props) => {
               onClick={() => {
                 pageHandler(8);
               }}
-              className={`${active === 8 ? "active" : ""}`}
+              className={`${active === 8 ? "active-element" : ""}`}
             >
               100
             </div>
