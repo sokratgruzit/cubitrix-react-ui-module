3b18a9d0131caf815a69e18aa2ed40dadf8b09f1
keksik95 <geliashvilidato95@gmail.com>
2023-01-25 20:08:20 +0400

    update

--- src/components/WithdrawalPopup/WithdrawalPopup.jsx
+++ src/components/WithdrawalPopup/WithdrawalPopup.jsx
@@ -8,46 +8,33 @@ export const WithdrawalPopup = (props) => {
       <div>
         <div className="withdrawal-container" style={props.customStyles}>
           <div className="withdrawal-inner">
-            <div className="flex jc-sb">
-              <div>#6379325951635a33347837434e334e6b345a6e3052513d3d</div>
-              <div>Approved</div>
-            </div>
-            <div className="flex jc-sb mt-22">
-              <div>#Withdraw Wallet (BSC)</div>
-              <div>0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2</div>
+            <div className="withdrawal">
+              <div className="flex jc-sb">
+                <div>{props.label}</div>
+                <div>Approved</div>
+              </div>
+              <div className="flex jc-sb mt-22">
+                <div>{props.wallet}</div>
+                <div>{props.code}</div>
+              </div>
             </div>
             <div className="withdrawal-details">
               <div className="withdrawal-details-inner">
-                <div className="withdraw-title font-16">Withdraw Details</div>
-                <div className="withdraw-wraper">
-                  <div className="popup-item flex jc-sb withdraw-bg">
-                    <div>Request By</div>
-                    <div>
-                      <div className="align-right">mariam</div>
-                      <div className="mt-8 font-12">
-                        mariamtalakhadze01@gmail.com
+                <div className="withdraw-title font-16">{props.title}</div>
+                <div className="withdrawal-wraper">
+                  {props.withdrawalData.map((item) => {
+                    return (
+                      <div key={item.id} className="popup-item flex jc-sb">
+                        <div>{item.name}</div>
+                        <div>
+                          <div className="align-right">{item.user}</div>
+                          <div className="mt-8 font-12 align-right">
+                            {item.sub}
+                          </div>
+                        </div>
                       </div>
-                    </div>
-                  </div>
-                  <div className="popup-item flex jc-sb">
-                    <div>Request At</div>
-                    <div>
-                      <div className="align-right">14.10.2021</div>
-                      <div className="mt-8 font-12 alight-right">04:00 AM</div>
-                    </div>
-                  </div>
-                  <div className="popup-item flex jc-sb withdraw-bg">
-                    <div>Tokens</div>
-                    <div>
-                      <div>1,250 CMCX</div>
-                    </div>
-                  </div>
-                  <div className="popup-item flex jc-sb">
-                    <div>Details</div>
-                    <div>
-                      <div>Tokens Withdraw</div>
-                    </div>
-                  </div>
+                    );
+                  })}
                 </div>
               </div>
             </div>
--- src/stories/WithdrawalPopup.stories.js
+++ src/stories/WithdrawalPopup.stories.js
@@ -8,6 +8,25 @@ const stories = storiesOf("WithdrawalPopup", module);
 let data = [
   {
     id: 0,
+    name: "Request By",
+    user: "mariam",
+    sub: "mariamtalakhadze01@gmail.com",
+  },
+  {
+    id: 1,
+    name: "Request At",
+    user: "14.10.2021",
+    sub: "04:00 AM",
+  },
+  {
+    id: 2,
+    name: "Tokens",
+    user: "1,250 CMCX",
+  },
+  {
+    id: 3,
+    name: "Details",
+    user: "Tokens Withdraw",
   },
 ];
 
@@ -28,6 +47,10 @@ stories.add("WithdrawalPopup", () => {
         }}
       />
       <WithdrawalPopup
+        label={"#6379325951635a33347837434e334e6b345a6e3052513d3d"}
+        wallet={"#Withdraw Wallet (BSC)"}
+        code={"0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2"}
+        title={"Withdraw Details"}
         type={"withdrawalPopup"}
         customStyles={{ width: "596px" }}
         withdrawalData={data}
