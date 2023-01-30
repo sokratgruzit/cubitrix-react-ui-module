205c17096e202dcc5800e9c466812dbe2ab5b371
chubinashvili <chubinashvili@pop-os.localdomain>
2023-01-30 16:49:54 +0400

    last

--- src/components/AdminPanel/AdminPanel.jsx
+++ src/components/AdminPanel/AdminPanel.jsx
@@ -17,7 +17,7 @@ export const AdminPanel = props => {
                 fontSize={'font-20'}
                 customStyles={{}}
             />
-            <FilterBoxs
+            <FilterBox
                 tableFilterData={props.tableFilterData}
                 setTableFilterOutcomingData={props.setTableFilterOutcomingData}
                 tableHeader={props.tableHeader}
