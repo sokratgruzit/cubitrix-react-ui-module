de102878c58b3b07979b06782d80df531bef3bf9
chubinashvili <chubinashvili@pop-os.localdomain>
2023-01-31 14:42:44 +0400

    last

--- src/components/Popup/Popup.jsx
+++ src/components/Popup/Popup.jsx
@@ -4,19 +4,12 @@ import { Visual } from "../Visual";
 export const Popup = (props) => {
   return (
     <div className="popup-bg">
-      <div className="popup-wraper">
+      <div className="popup-wraper" >
         <Visual
           label={props.label}
           element={"popup-header"}
           customStyles={{
-            border: "2px solid #202431",
-            borderBottom: "none",
-            width: "100%",
-            background: "#141726",
-            borderBottomLeftRadius: "0",
-            borderBottomRightRadius: "0",
-            borderTopRightRadius: "8px",
-            borderTopLeftRadius: "8px",
+            width: "100%"
           }}
         />
         {props.body}
