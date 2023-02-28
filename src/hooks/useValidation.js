a1853a7f39746d4ea2e8a382b0d8580a6c595d9d
gogaokradze <deathofsuper@gmail.com>
2023-02-28 17:55:19 +0400

    fix: hash validation

--- src/hooks/useValidation.js
+++ src/hooks/useValidation.js
@@ -1,26 +1,32 @@
 export const useValidation = (formData, helpTexts) => {
-    const errors = {};
+  const errors = {};
 
-    const validation = {
-      email: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
-      password: /(?=(.*[0-9]))(?=.*[a-z])(?=(.*[A-Z]))(?=(.*)).{8,}/,
-      numbers: /^[0-9\b]+$/,
-      limitedCharacters: /^.{16}$/,
-      hash: /^[a-f0-9]{32}/,
-      number: /^\d+(?:\.\d+)?$/,
-    };
+  const validation = {
+    email: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
+    password: /(?=(.*[0-9]))(?=.*[a-z])(?=(.*[A-Z]))(?=(.*)).{8,}/,
+    numbers: /^[0-9\b]+$/,
+    limitedCharacters: /^.{16}$/,
+    hash: /^[a-fA-F0-9]{32}/i,
+    number: /^\d+(?:\.\d+)?$/,
+  };
 
-    Object.keys(formData).map(key => {
-        if (!validation[helpTexts[key].validationType].test(formData[key]) && formData[key].length > 0) {
-            errors[key] = {
-                failure: helpTexts[key].failure
-            };
-        } else if (validation[helpTexts[key].validationType].test(formData[key]) && formData[key].length > 0) {
-            errors[key] = {
-                success: helpTexts[key].success
-            };
-        };
-    });
+  Object.keys(formData).map((key) => {
+    if (
+      !validation[helpTexts[key].validationType].test(formData[key]) &&
+      formData[key].length > 0
+    ) {
+      errors[key] = {
+        failure: helpTexts[key].failure,
+      };
+    } else if (
+      validation[helpTexts[key].validationType].test(formData[key]) &&
+      formData[key].length > 0
+    ) {
+      errors[key] = {
+        success: helpTexts[key].success,
+      };
+    }
+  });
 
-    return errors;
+  return errors;
 };
