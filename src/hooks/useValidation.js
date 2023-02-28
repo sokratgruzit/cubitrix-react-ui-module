91995f793d469e7a13ca0df9722ec9a591c5ab19
Zura Chubinashvili <chubinashvili2003@gmail.com>
2023-02-28 14:24:23 +0400

    feat: add validation to DeveloperApi

--- src/hooks/useValidation.js
+++ src/hooks/useValidation.js
@@ -1,5 +1,5 @@
 export const useValidation = (formData, helpTexts) => {
-    const errors = {};
+    let errors = {};
 
     const validation = {
       email: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
@@ -11,16 +11,22 @@ export const useValidation = (formData, helpTexts) => {
       text: /^[a-zA-Z]+$/,
     };
 
-    Object.keys(formData).map(key => {
-        if (!validation[helpTexts[key].validationType].test(formData[key]) && formData[key].length > 0) {
-            errors[key] = {
-                failure: helpTexts[key].failure
-            };
-        } else if (validation[helpTexts[key].validationType].test(formData[key]) && formData[key].length > 0) {
-            errors[key] = {
-                success: helpTexts[key].success
-            };
+    Object.keys(formData).map((key) => {
+      if (
+        !validation[helpTexts[key].validationType].test(formData[key]) &&
+        formData[key].length > 0
+      ) {
+        errors[key] = {
+          failure: helpTexts[key].failure,
         };
+      } else if (
+        validation[helpTexts[key].validationType].test(formData[key]) &&
+        formData[key].length > 0
+      ) {
+        errors[key] = {
+          success: helpTexts[key].success,
+        };
+      }
     });
 
     return errors;
