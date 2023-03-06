0e0c61ad35f5ccd92c731edf80d57bb585f4c28c
gogaokradze <deathofsuper@gmail.com>
2023-03-06 15:30:46 +0400

    feat: created loan table

--- src/components/Loan/Loan.jsx
+++ src/components/Loan/Loan.jsx
@@ -3,6 +3,9 @@ import React, { useState } from "react";
 import { useMobileWidth } from "../../hooks/useMobileWidth";
 import { Button } from "../Button";
 import "./Loan.css";
+import { DashboardBalance } from "../DashboardBalance";
+import { Table } from "../Table";
+import { InfoCircleIcon } from "../../assets/svgs";
 
 export const Loan = () => {
   const { width } = useMobileWidth();
@@ -34,13 +37,8 @@ export const Loan = () => {
             </div>
           </div>
           <div className="loan-bar-body">
-            <div className="earnings-circle">
-              <div className="earning-circle-inner">
-                <h3>Eaning</h3>
-                <p className="earning-amount-token">$8.193010001</p>
-                <p className="earning-amount-USDC">2.22004 USDC</p>
-              </div>
-            </div>
+            <DashboardBalance earning={"$8.193010001"} usdc={"2.22004 USDC"} />
+
             <div className="buttons-wrapper">
               <Button
                 label={"Supply USDC "}
@@ -61,7 +59,100 @@ export const Loan = () => {
                 customStyles={{ margin: "0" }}
               />
             </div>
+            <div className="position-summary">
+              <h3>Position Summary</h3>
+              <div className="position-summary-point">
+                <p>Collateral Value</p>
+                <p>$0.00</p>
+              </div>
+              <div className="position-summary-point">
+                <p>Liquidation Point</p>
+                <p>$0.00</p>
+              </div>
+              <div className="position-summary-point">
+                <p>Borrow Capacity</p>
+                <p>$0.00</p>
+              </div>
+              <div className="position-summary-point">
+                <p>Available to Borrow</p>
+                <p>$0.00</p>
+              </div>
+            </div>
           </div>
+        </div>
+      </div>
+      <div className={"main-content"}>
+        <h2>Loan</h2>
+        <Table
+          type={"table-version"}
+          tableHead={[
+            {
+              name: "Collateral Asset",
+              width: 50,
+              mobileWidth: 50,
+              id: 0,
+            },
+            {
+              name: "Protocol Balance",
+              width: 50,
+              mobileWidth: 50,
+
+              id: 1,
+            },
+          ]}
+          mobile={width < 1280}
+          tableData={[
+            {
+              id: 0,
+              name: "Chainlink",
+              shortName: "LINK",
+              amount: "0.00",
+              protocolBalance: "0.0000",
+            },
+          ].map((item, index) => {
+            return (
+              <div className={`table-parent loan-pool-row`} key={index}>
+                <div className="info-circle">
+                  <InfoCircleIcon />
+                </div>
+                <div className="collateral-name">img {item.name}</div>
+                <div className="collateral-shortName">
+                  <p>{item.shortName}</p>
+                  <p>{item.amount} in wallet</p>
+                </div>
+                <div className="protocol-balance">{item.protocolBalance}</div>
+                <div className="pool-loan-actions">
+                  <div>
+                    <span>-</span>
+                  </div>
+                  <div>
+                    <span>+</span>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        />
+        <div>
+          <div>
+            <span>Collateral Asset</span>
+            <span>Protocol Balance</span>
+          </div>
+          {[].map((asset, index) => {
+            return (
+              <div>
+                <div></div>
+                <div>
+                  img
+                  {asset.title}
+                </div>
+                <div>
+                  <p>{asset?.shortName}</p>
+                  <p>{asset?.amount} in wallet</p>
+                </div>
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
