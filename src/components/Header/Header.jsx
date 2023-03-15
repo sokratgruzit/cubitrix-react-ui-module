72ec7f0918a372ec7ddb5ecbba96c4d37c648d9d
Zura Chubinashvili <chubinashvili2003@gmail.com>
2023-03-15 15:34:23 +0400

    fix: header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -14,7 +14,6 @@ import {
 // styles
 import "./Header.css";
 
-
 export const Header = ({
   modules,
   account,
@@ -32,29 +31,29 @@ export const Header = ({
   let mobile = width <= 970;
 
   return (
-    <div className="header">
-      <div className="modulesWrapper">
-        <div className="logoWrapper">
-          {logoSvg}
-          <h3 style={{ display: `${width <= 1100 ? 'none' : 'block'}`}}>{title}</h3>
+    <div className={`header ${mobile && navbarActive ? 'header-active' : ''}`}>
+        <div className="modulesWrapper">
+          <div className="logoWrapper">
+            {logoSvg}
+            <h3 style={{ display: `${width <= 1100 ? 'none' : 'block'}`}}>{title}</h3>
+          </div>
+          {!mobile && <NavbarHelper type={'navbar'} modules={modules} />}
+          <div style={{ display: 'flex', alignItems: 'center'}}>
+            {mobile && <NavbarHelper type={"notification"} onClick={handleNotifications} sideBar={sideBar} sideBarOpen={sideBarOpen} modules={modules} />}
+            {mobile && <NavbarHelper type={'connect'} onClick={handleConnect} account={account} verified={verified} />}
+            {mobile && (
+              <div className={`navbar-menu`} onClick={() => setNavbarActive(prev => !prev)}>
+                <p className={`${navbarActive ? 'active' : ''} font-12`}>Close</p>
+                <Menu active={navbarActive} />
+              </div>
+            )}
+          </div>
         </div>
-        {!mobile && <NavbarHelper type={'navbar'} modules={modules} />}
-        <div style={{ display: 'flex', alignItems: 'center'}}>
-          {mobile && <NavbarHelper type={"notification"} onClick={handleNotifications} sideBar={sideBar} sideBarOpen={sideBarOpen} modules={modules} />}
-          {mobile && <NavbarHelper type={'connect'} onClick={handleConnect} account={account} verified={verified} />}
-          {mobile && (
-            <div className={`navbar-menu`} onClick={() => setNavbarActive(prev => !prev)}>
-              <p className={`${navbarActive ? 'active' : ''} font-12`}>Close</p>
-              <Menu active={navbarActive} />
-            </div>
-          )}
+        <div className={`right ${navbarActive ? 'right-active' : ''}`}>
+          {!mobile && <NavbarHelper type={"notification"} onClick={handleNotifications} sideBar={sideBar} sideBarOpen={sideBarOpen} modules={modules} />}
+          {!mobile && <NavbarHelper type={'connect'} onClick={handleConnect} account={account} verified={verified} />}
+          {mobile && <NavbarHelper type={'navbar'} modules={modules} />}
         </div>
-      </div>
-      <div className={`right ${navbarActive ? 'right-active' : ''}`}>
-        {!mobile && <NavbarHelper type={"notification"} onClick={handleNotifications} sideBar={sideBar} sideBarOpen={sideBarOpen} modules={modules} />}
-        {!mobile && <NavbarHelper type={'connect'} onClick={handleConnect} account={account} verified={verified} />}
-        {mobile && <NavbarHelper type={'navbar'} modules={modules} />}
-      </div>
     </div>
   );
 };
