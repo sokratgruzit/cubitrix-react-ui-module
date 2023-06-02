ab07567be8d882b2caf2fe4439c4949cd88e6da7
gogaokradze <deathofsuper@gmail.com>
2023-06-02 17:10:17 +0400

    landing

--- src/components/Landing/components/index.js
+++ src/components/Landing/components/index.js
@@ -3,3 +3,4 @@ export { Meditation } from "./Meditation/Meditation";
 export { DashboardHeader } from "./DashboardHeader/DashboardHeader";
 export { TopCoins } from "./TopCoins/TopCoins";
 export { WhyComplend } from "./WhyComplend/WhyComplend";
+export { Overview } from "./Overview/Overview";
