4f6b10e4fbbac2de45cbd727cf7a40cd29b91c6a
sergo93@gmail.com <sergo93@gmail.com>
2023-07-17 11:48:58 +0400

    last

--- src/components/Landing/components/DashboardHeader/DashboardHeader.js
+++ src/components/Landing/components/DashboardHeader/DashboardHeader.js
@@ -1,5 +1,4 @@
-import React, { useState, useLayoutEffect } from "react";
-
+import React, { useState,useEffect,useRef, useLayoutEffect } from "react";
 // components
 import { Button } from "../../../Button";
 
@@ -14,57 +13,59 @@ export const DashboardHeader = ({
   animate,
 }) => {
   return (
-    <header className={`dashboard-header-container ${animate ? "animate" : ""}`}>
-      <img
-        src={dashboardHeaderImages?.rocket}
-        className={"dashboard-header-rocket-img"}
-      />
-      <div className="dashboard-header">
-        <img src={dashboardHeaderImages?.dots} className={"dashboard-header-dots-img"} />
-        <div className={"dashboard-header-top"}>
-          <h1>Buy</h1>
-          <h1>Sell & Trade</h1>
-        </div>
-        <div className={"dashboard-header-bottom"}>
-          <h1>Crypto</h1>
-          <p>Trade, buy, staking and loan cryptocurrency at Complend</p>
-        </div>
-        <img src={dashboardHeaderImages?.man} className={"dashboard-header-man-img"} />
-        <img
-          src={dashboardHeaderImages?.planet}
-          className={"dashboard-header-planet-img"}
-        />
-      </div>
-      <img
-        src={dashboardHeaderImages?.dotsRight}
-        className={"dashboard-header-dotsRight-img"}
-      />
-      <div className={"dashboard-buttons"}>
-        {!account && (
-          <Button
-            label={"Connect Wallet"}
-            size={"btn-lg"}
-            type={"btn-primary"}
-            arrow={"arrow-none"}
-            element={"button"}
-            onClick={handleConnect}
-            customStyles={{ margin: "0" }}
-          />
-        )}
-        <Button
-          label={"Get Started"}
-          size={"btn-lg"}
-          type={"btn-secondary"}
-          arrow={"arrow-none"}
-          element={"button"}
-          onClick={handleGetStarted}
-          customStyles={{ margin: "0" }}
-        />
-      </div>
-      <img
-        src={dashboardHeaderImages?.bottom}
-        className={"dashboard-header-bottom-img"}
-      />
-    </header>
+          <header className={`h-screen dashboard-header-container ${animate ? "animate" : ""}`}>
+              <img
+                  src={dashboardHeaderImages?.rocket}
+                  className={"dashboard-header-rocket-img"}
+              />
+              <div className="dashboard-header">
+                  <img src={dashboardHeaderImages?.dots} className={"dashboard-header-dots-img"} />
+                  <div className={"dashboard-header-top"}>
+                      <h1>Buy</h1>
+                      <h1>Sell & Trade</h1>
+                  </div>
+                  <div className={"dashboard-header-bottom"}>
+                      <h1>Crypto</h1>
+                      <p>Trade, buy, staking and loan cryptocurrency at Complend</p>
+                  </div>
+                  <img src={dashboardHeaderImages?.man} className={"dashboard-header-man-img"} />
+                  <img
+                      src={dashboardHeaderImages?.planet}
+                      className={"dashboard-header-planet-img"}
+                  />
+              </div>
+              <img
+                  src={dashboardHeaderImages?.dotsRight}
+                  className={"dashboard-header-dotsRight-img"}
+              />
+              <div className={"dashboard-buttons"}>
+                  {!account && (
+                      <Button
+                          label={"Connect Wallet"}
+                          size={"btn-lg"}
+                          type={"btn-primary"}
+                          arrow={"arrow-none"}
+                          element={"button"}
+                          onClick={handleConnect}
+                          customStyles={{ margin: "0" }}
+                      />
+                  )}
+                  <Button
+                      label={"Get Started"}
+                      size={"btn-lg"}
+                      type={"btn-secondary"}
+                      arrow={"arrow-none"}
+                      element={"button"}
+                      onClick={handleGetStarted}
+                      customStyles={{ margin: "0" }}
+                  />
+              </div>
+              <img
+                  src={dashboardHeaderImages?.bottom}
+                  className={"dashboard-header-bottom-img"}
+              />
+          </header>
+
+
   );
 };
