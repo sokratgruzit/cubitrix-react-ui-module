320dad8cc1cd50f7a368c525ef3376db803f0c38
sergo93@gmail.com <sergo93@gmail.com>
2023-07-26 13:55:11 +0400

    add referral ack btn disable

--- src/components/Referral/Referral.jsx
+++ src/components/Referral/Referral.jsx
@@ -50,6 +50,7 @@ export const Referral = ({
   referralHistoryButtonsRight,
   referralHistoryTableType,
   referralTableTitle,
+  referralBackActive
 }) => {
   const [mobileExpand, setMobileExpand] = useState(null);
   const [treeInfo, setTreeInfo] = useState(null);
@@ -557,23 +558,24 @@ export const Referral = ({
                               referralTreeUserBackClick();
                             }}
                           >
-                            <div className="referral-tree-item-level-active-back">
+                            {referralBackActive && (<div className="referral-tree-item-level-active-back">
                               <span>
                                 <svg
-                                  width="14"
-                                  height="14"
-                                  viewBox="0 0 14 14"
-                                  fill="none"
-                                  xmlns="http://www.w3.org/2000/svg"
+                                    width="14"
+                                    height="14"
+                                    viewBox="0 0 14 14"
+                                    fill="none"
+                                    xmlns="http://www.w3.org/2000/svg"
                                 >
                                   <path
-                                    d="M10.4527 5.64648H3.54603C2.98603 5.64648 2.70603 6.32315 3.1027 6.71982L6.12436 9.74148C6.60853 10.2257 7.39603 10.2257 7.8802 9.74148L9.02937 8.59232L10.9019 6.71982C11.2927 6.32315 11.0127 5.64648 10.4527 5.64648Z"
-                                    fill="#C38C5C"
+                                      d="M10.4527 5.64648H3.54603C2.98603 5.64648 2.70603 6.32315 3.1027 6.71982L6.12436 9.74148C6.60853 10.2257 7.39603 10.2257 7.8802 9.74148L9.02937 8.59232L10.9019 6.71982C11.2927 6.32315 11.0127 5.64648 10.4527 5.64648Z"
+                                      fill="#C38C5C"
                                   />
                                 </svg>
                                 Back
                               </span>
-                            </div>
+                              </div>)
+                            }
                             <div className="referral-tree-btn-out">
                               <div className={`referral-tree-btn-img`}>
                                 <img
