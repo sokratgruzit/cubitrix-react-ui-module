dfef8974523a025406747b6d925b24e12ea43167
whatever <eqiza99@gmail.com>
2023-09-07 17:11:37 +0400

    upgrade fixed version

--- src/components/LandingSteps/LandingSteps.jsx
+++ src/components/LandingSteps/LandingSteps.jsx
@@ -50,6 +50,7 @@ export const LandingSteps = ({
   referralState,
   setReferralState,
   amountProgressValue,
+  setAmountProgressValue,
   amountProgressOnchange,
   handleFinish,
   referralCodeChecked,
