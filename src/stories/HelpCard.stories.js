d2d51b24069a712a555b2fa805c2fd696fbf3b42
aLLod1n <bekalichava22@gmail.com>
2023-12-26 11:26:14 +0400

    update

--- src/stories/HelpCard.stories.js
+++ src/stories/HelpCard.stories.js
@@ -14,6 +14,7 @@ stories.add("HelpCard", (props) => {
 
   return (
     <div>
+    <button onClick={() => setActive(!active)}>success</button>
       <HelpCard
         result={"success"}
         text={
