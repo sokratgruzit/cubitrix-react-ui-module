9789568c9089dac936aac8055213c3dd21e2ccdf
aLLod1n <bekalichava22@gmail.com>
2024-01-03 19:51:49 +0400

    update

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -71,33 +71,35 @@ export const Header = ({
           <h3>{title}</h3>
         </div>
         {!mobile && <NavbarHelper type={"navbar"} modules={modules} />}
-        {location.pathname === "/trade" && !mobile ? (
-          <>
-            <TradeBar
-              tradePriceData={tradePriceData}
-              showBalance={showBalance}
-              setShowBalance={setShowBalance}
-            />
-            <div
-              onClick={() => handleShowBalance()}
-              className="tabBarIcon"
-            ></div>
-          </>
-        ) : null}
-        <div className="a1-price">
-          <svg
-            width="61"
-            height="62"
-            viewBox="0 0 61 62"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M46.0358 6.91431L0.497925 53.3785H7.84852H14.7808H26.4939L29.3027 43.488H24.1633L48.8744 17.3426L38.8346 53.3785H49.3226L60.4979 6.91431H46.0358Z"
-              fill="white"
-            ></path>
-          </svg>
-          A1 Price - {A1Price} $
+        <div className="a1-price-and-mask">
+          {location.pathname === "/trade" && !mobile ? (
+            <>
+              <TradeBar
+                tradePriceData={tradePriceData}
+                showBalance={showBalance}
+                setShowBalance={setShowBalance}
+              />
+              <div
+                onClick={() => handleShowBalance()}
+                className="tabBarIcon"
+              ></div>
+            </>
+          ) : null}
+          <div className="a1-price">
+            <svg
+              width="61"
+              height="62"
+              viewBox="0 0 61 62"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M46.0358 6.91431L0.497925 53.3785H7.84852H14.7808H26.4939L29.3027 43.488H24.1633L48.8744 17.3426L38.8346 53.3785H49.3226L60.4979 6.91431H46.0358Z"
+                fill="white"
+              ></path>
+            </svg>
+            A1 Price - {A1Price} $
+          </div>
         </div>
         <div style={{ display: "flex", alignItems: "center" }}>
           {mobile && (
