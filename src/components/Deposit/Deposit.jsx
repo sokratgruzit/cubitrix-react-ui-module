7d7dd9da4eb5094fcf7e622f9368b67616fa4c73
aLLod1n <bekalichava22@gmail.com>
2024-02-02 18:44:57 +0400

    getbalance

--- src/components/Deposit/Deposit.jsx
+++ src/components/Deposit/Deposit.jsx
@@ -21,26 +21,9 @@ export const Deposit = ({
   depositLoading,
   library,
   account,
-  getBalance,
+  tokenBalance,
+  providerType,
 }) => {
-  const [tokenBalance, setTokenBalance] = useState("");
-  useEffect(() => {
-    const fetchBalance = () => {
-      if (library && account) {
-        getBalance()
-          .then((res) => {
-            setTokenBalance(res);
-          })
-          .catch((err) => {
-            console.error(`Error fetching balance: ${err}`);
-          });
-      }
-    };
-    fetchBalance();
-    const intervalId = setInterval(fetchBalance, 10000);
-    return () => clearInterval(intervalId);
-  }, [library, account]);
-
   const handleInputChange = (e, params) => {
     const { name, onChange } = params;
 
@@ -91,50 +74,59 @@ export const Deposit = ({
                 let selectedOption;
                 if (params.type === "lable-input-select") {
                   selectedOption = params?.options.find(
-                    (option) => option.value === currentObject[params?.name],
+                    (option) => option.value === currentObject[params?.name]
                   );
                 }
 
                 return (
-                    <div className="withdraw-to-acc-input-wrapper" key={index}>
-                      <Input
-                        key={index}
-                        type={params?.type}
-                        inputType={params?.inputType}
-                        label={params.title}
-                        name={params.name}
-                        disabled={params.disabled}
-                        value={
-                          params?.type === "lable-input-select"
-                            ? selectedOption?.name ||
-                              params?.defaultAny ||
-                              params?.options[0]?.value
-                            : currentObject[params?.name] || params?.defaultAny
-                        }
-                        customStyles={{ width: "100%" }}
-                        selectHandler={(opt) => {
-                          handleInputChange(opt, params);
-                        }}
-                        placeholder={params?.placeholder}
-                        onChange={(e) => handleInputChange(e, params)}
-                        defaultData={params?.options}
-                        customInputStyles={{ border: "1px solid rgba(255, 255, 255, 0.1)" }}
-                        svg={
-                          params?.type === "lable-input-select"
-                            ? selectedOption?.svg
-                            : params?.svg
-                        }
-                      />
-                      {params?.rightText && (
-                          <span className="font-14 withdraw-to-acc-input-right">
-                            {params?.rightText}
-                          </span>
-                      )}
-                    </div>
+                  <div className="withdraw-to-acc-input-wrapper" key={index}>
+                    <Input
+                      key={index}
+                      type={params?.type}
+                      inputType={params?.inputType}
+                      label={params.title}
+                      name={params.name}
+                      disabled={params.disabled}
+                      value={
+                        params?.type === "lable-input-select"
+                          ? selectedOption?.name ||
+                            params?.defaultAny ||
+                            params?.options[0]?.value
+                          : currentObject[params?.name] || params?.defaultAny
+                      }
+                      customStyles={{ width: "100%" }}
+                      selectHandler={(opt) => {
+                        handleInputChange(opt, params);
+                      }}
+                      placeholder={params?.placeholder}
+                      onChange={(e) => handleInputChange(e, params)}
+                      defaultData={params?.options}
+                      customInputStyles={{
+                        border: "1px solid rgba(255, 255, 255, 0.1)",
+                      }}
+                      svg={
+                        params?.type === "lable-input-select"
+                          ? selectedOption?.svg
+                          : params?.svg
+                      }
+                    />
+                    {params?.rightText && (
+                      <span className="font-14 withdraw-to-acc-input-right">
+                        {params?.rightText}
+                      </span>
+                    )}
+                  </div>
                 );
               })}
             </div>
-            {info && <HelpText title={info} status="info" color="#6A6D76" icon={true} />}
+            {info && (
+              <HelpText
+                title={info}
+                status="info"
+                color="#6A6D76"
+                icon={true}
+              />
+            )}
           </div>
           <Button
             label={buttonLabel}
