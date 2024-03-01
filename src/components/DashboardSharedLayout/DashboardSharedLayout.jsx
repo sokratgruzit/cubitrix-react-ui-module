d29f00e379c5752ba4e4d6de2a502ef9d7b4c66b
aLLod1n <bekalichava22@gmail.com>
2024-03-01 15:21:19 +0400

    update

--- src/components/DashboardSharedLayout/DashboardSharedLayout.jsx
+++ src/components/DashboardSharedLayout/DashboardSharedLayout.jsx
@@ -74,8 +74,8 @@ export const DashboardSharedLayout = ({
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 24 24"
-                  width="30"
-                  height="20"
+                  width="24"
+                  height="24"
                 >
                   <rect
                     x="2"
